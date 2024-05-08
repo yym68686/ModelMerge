11be4868d657124c2c31cd854156e935f8add0d4
yym68686 <yym68686@outlook.com>
2024-05-08 18:11:23 +0800

    update name

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from pathlib import Path
 from setuptools import setup, find_packages
 
 setup(
-    name="LLM-Hub",
-    version="0.1.3",
-    description="LLM-Hub is a multi-large language model API aggregator.",
+    name="ModelMerge",
+    version="0.1.4",
+    description="ModelMerge is a multi-large language model API aggregator.",
     long_description=Path.open(Path("README.md"), encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     packages=find_packages("models"),
