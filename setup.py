b4bb99b052ab38e830cf34adbdc9e7524bbcaa18
yym68686 <yym68686@outlook.com>
2024-05-08 16:13:32 +0800

    update setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="LLM-Hub",
-    version="0.1.0",
+    version="0.1.1",
     description="LLM-Hub is a multi-large language model API aggregator.",
     long_description=Path.open(Path("README.md"), encoding="utf-8").read(),
     long_description_content_type="text/markdown",
