f0208c895251073a90afea494acfb386469fa868
yym68686 <yym68686@outlook.com>
2024-05-09 01:22:00 +0800

    update setup

--- setup.py
+++ setup.py
@@ -4,11 +4,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="ModelMerge",
-    version="0.1.4",
+    version="0.1.5",
     description="ModelMerge is a multi-large language model API aggregator.",
     long_description=Path.open(Path("README.md"), encoding="utf-8").read(),
     long_description_content_type="text/markdown",
-    packages=find_packages("models"),
-    package_dir={"": "models"},
+    packages=find_packages(where=["models", "tools", "utils"]),
+    package_dir={"": "."},
+    install_requires=Path.open(Path("requirements.txt"), encoding="utf-8").read().splitlines(),
     # py_modules=["LLM-Hub"]
 )
