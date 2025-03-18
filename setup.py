6330f898d7af541df9ffc9b358c9ec3ba43cc1c4
yym68686 <yym68686@outlook.com>
2025-03-18 11:55:32 +0800

    🔄 Update: Bump version to 1.0.10 in setup.py, include package data, and enable recursive submodule checkout in GitHub Actions workflow for improved dependency management.

--- setup.py
+++ setup.py
@@ -4,11 +4,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="modelmerge",
-    version="1.0.9",
+    version="1.0.10",
     description="modelmerge is a multi-large language model API aggregator.",
     long_description=Path.open(Path("README.md"), encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     packages=find_packages("src"),
     package_dir={"": "src"},
     install_requires=Path.open(Path("requirements.txt"), encoding="utf-8").read().splitlines(),
+    include_package_data=True,
 )
