738655d615a5ca9f351b07d5bee2c0b475bb3dde
yym68686 <yym68686@outlook.com>
2025-03-23 06:59:18 +0800

    Update subproject reference in core module

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="aient",
-    version="1.0.30",
+    version="1.0.31",
     description="Aient: The Awakening of Agent.",
     long_description=Path.open(Path("README.md"), encoding="utf-8").read(),
     long_description_content_type="text/markdown",
